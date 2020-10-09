107a255a6945c03f04c477578ea910474e34ee29
adsf117 <@Thecats.05>
2020-10-09 14:13:20 -0500

    UI fix show times

--- app/src/main/java/com/puzzlebench/rappelmoi/form/FormReminderActivity.kt
+++ app/src/main/java/com/puzzlebench/rappelmoi/form/FormReminderActivity.kt
@@ -14,13 +14,18 @@ import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.Constraints
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.puzzlebench.rappelmoi.R
 import com.puzzlebench.rappelmoi.components.AlarmScheduler
+import com.puzzlebench.rappelmoi.components.EventWorker
 import com.puzzlebench.rappelmoi.database.Event
 import com.puzzlebench.rappelmoi.database.RappelMoiDatabase
 import com.puzzlebench.rappelmoi.formatDate
 import kotlinx.android.synthetic.main.activity_form_reminder.*
 import kotlinx.android.synthetic.main.activity_form_reminder.tv_date
+import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -47,8 +52,10 @@ class FormReminderActivity : AppCompatActivity(), DatePickerDialog.OnDateSetList
         val viewModelFactory = FromReminderViewModelFactory(dataSource)
         viewModel = ViewModelProvider(this, viewModelFactory).get(FromReminderViewModel::class.java)
         val calendar: Calendar = Calendar.getInstance()
-        tv_date.text =calendar.timeInMillis.formatDate("MM dd yyyy")
-        tv_time.text = calendar.timeInMillis.formatDate("hh:mm")
+        calendar.run {
+            showDate(this.timeInMillis)
+            showTime(this.timeInMillis)
+        }
         viewModel.viewStateLiveData.observe(::getLifecycle, ::handleViewState)
 
         createNotificationChannel(
@@ -138,10 +145,21 @@ class FormReminderActivity : AppCompatActivity(), DatePickerDialog.OnDateSetList
             ).show()
         }
     }
-    private fun useWork(event:Event){
 
+    private fun useWork(event: Event) {
+        val workerConstraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true).build()
+
+        val eventWorker = OneTimeWorkRequestBuilder<EventWorker>()
+            .setConstraints(workerConstraints)
+            .setInputData(EventWorker.setEventWorkerData(event.id))
+            .build()
+
+        val workManager = WorkManager.getInstance(this.applicationContext)
+        workManager.enqueue(eventWorker)
 
     }
+
     private fun showDatePickerDialog() {
         val calendar: Calendar = Calendar.getInstance()
         val datePickerDialog = DatePickerDialog(
@@ -161,16 +179,32 @@ class FormReminderActivity : AppCompatActivity(), DatePickerDialog.OnDateSetList
         val calendar: Calendar = Calendar.getInstance()
         val timePickerDialog = TimePickerDialog(
             this, this, calendar.get(Calendar.HOUR), calendar.get(Calendar.MINUTE),
-            DateFormat.is24HourFormat(this)
+            true
         )
         timePickerDialog.show()
     }
 
+    private fun showDate(date: Long) {
+        tv_date.text = date.formatDate("MM dd yyyy")
+    }
+
+    private fun showTime(dateTime: Long) {
+        tv_time.text = dateTime.formatDate("HH:mm")
+
+    }
+
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
-        tv_date.text = "$day $month $year"
+        val date = Calendar.getInstance()
+        date.set(Calendar.YEAR, year)
+        date.set(Calendar.MONTH, month)
+        date.set(Calendar.DAY_OF_MONTH, day)
+        showDate(date.timeInMillis)
     }
 
     override fun onTimeSet(view: TimePicker?, hour: Int, minute: Int) {
-        tv_time.text = "$hour:$minute"
+        val datetime = Calendar.getInstance()
+        datetime.set(Calendar.HOUR_OF_DAY, hour)
+        datetime.set(Calendar.MINUTE, minute)
+        showTime(datetime.timeInMillis)
     }
 }
